df940bfbe803e03db5e45959972a238b5689d159
AhmadDFN <acmadauliai@example.com>
2024-05-15 13:34:52 +0700

    Dashboard Finish

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Hutang;
 use App\Models\Pelanggan;
+use App\Models\Produk;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -15,15 +18,35 @@ class DashboardController extends Controller
 
     public function index()
     {
+        $rev_year = DB::select("SELECT SUM(trans_gtotal - trans_ppn) AS total 
+                    FROM transaksis 
+                    WHERE strftime('%Y', trans_tanggal) = strftime('%Y', 'now')");
+        $rev_month = DB::select("SELECT SUM(trans_gtotal - trans_ppn) AS total 
+                    FROM transaksis 
+                    WHERE strftime('%Y-%m', trans_tanggal) = strftime('%Y-%m', 'now')");
+        $rev_week = DB::select("SELECT SUM(trans_gtotal - trans_ppn) AS total
+                    FROM transaksis
+                    WHERE strftime('%Y-%W', trans_tanggal) = strftime('%Y-%W', 'now')
+                    GROUP BY strftime('%Y-%W', trans_tanggal)");
+        $rev_day = DB::select("SELECT SUM(trans_gtotal - trans_ppn) AS total
+                    FROM transaksis
+                    WHERE strftime('%Y-%m-%d', trans_tanggal) = date('now')");
+        $hutang = DB::select("SELECT SUM(trans_gtotal) AS total FROM transaksis WHERE pembayaran='Hutang'");
+
         $data = [
+            "total_user" => User::count(),
+            "total_pelanggan" => Pelanggan::count(),
+            "total_produk" => Produk::count(),
+            "total_transaksi" => Transaksi::count(),
+            "rev_year" => $rev_year[0],
+            "rev_month" => $rev_month[0],
+            "rev_week" => $rev_week ? $rev_week[0]->total : 0,
+            "rev_day" => $rev_day[0],
+            "hutang" => $hutang[0],
             "title" => "Dashboard",
             'page' => 'Dashboard',
-            "pelanggans" => Pelanggan::All(),
-            'transaksis' => Transaksi::All(),
-            'hutangs' => Hutang::All(),
-            'add' => $this->route . "create",
-            'index' => $this->route,
         ];
+
         // dd($data);
         return view("dashboard", $data);
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,5 +4,140 @@
 @section('page-title', $page)
 
 @section('content')
+    <style>
+        body {
+            color: black
+        }
+    </style>
+    <div class="text-center" style="color: black">
+        <h5><strong>Siregas</strong></h3>
+            <p><strong>JL in aja dulu no 1 Semarang</strong></p>
+            <hr>
+    </div>
+    <div class="row">
+        <div class="col-12 col-sm-4 col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Pelanggan</span>
+                    <span class="info-box-number">
+                        {{ $total_pelanggan }}
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <div class="col-12 col-sm-4 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-danger elevation-1"><img src="{{ asset('dashmin/img/gasicon.ico') }}"
+                        alt="Icon Gas" width="32" height="32"></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Produk</span>
+                    <span class="info-box-number">{{ $total_produk }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Transaksi</span>
+                    <span class="info-box-number">{{ $total_transaksi }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-user"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Total User</span>
+                    <span class="info-box-number">{{ $total_user }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+    </div>
+    <h3>Pendapatan</h3>
+    <div class="row">
+        <div class="col-12 col-sm-4 col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-dollar-sign"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Pendapatan Per Tahun</span>
+                    <span class="info-box-number">
+                        Rp {{ number_format($rev_year->total, '0', ',', '.') }}
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <div class="col-12 col-sm-4 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-dollar-sign"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Pendapatan Per Bulan</span>
+                    <span class="info-box-number"> Rp {{ number_format($rev_month->total, '0', ',', '.') }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <div class="col-12 col-sm-4 col-md-3">
+            <div class="info-box mb-6">
+                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-dollar-sign"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Pendapatan Per Minggu</span>
+                    <span class="info-box-number">Rp {{ number_format($rev_week, '0', ',', '.') }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <div class="col-12 col-sm-4 col-md-3">
+            <div class="info-box mb-6">
+                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-dollar-sign"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Pendapatan Per Hari</span>
+                    <span class="info-box-number">Rp {{ number_format($rev_day->total, '0', ',', '.') }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+    </div>
+    <h3>Piutang</h3>
+    <div class="row">
+        <div class="col-3">
+
+        </div>
+        <div class="col-6">
+            <div class="info-box mb-6">
+                <span class="info-box-icon bg-dark elevation-1"><i class="fas fa-dollar-sign"
+                        style="color: white"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Total Hutang</span>
+                    <span class="info-box-number">Rp {{ number_format($hutang->total, '0', ',', '.') }}</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+    </div>
 
 @endsection
